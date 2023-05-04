ba240e5b75704511ea59e7bda2e5a052b74bc485
Lucas Gonzaga <lucassoft@gmx.com>
2023-05-03 23:32:24 -0300

    att projeto
    Mudança de pastas

--- script.js
+++ script.js
@@ -15,19 +15,19 @@ function verificar(){
             genero = 'Homem'
             if(idade >= 0 && idade < 10){
                 //bebe-criança
-                img.src = 'homembebe.png'
+                img.src = 'imagens/homembebe.png'
                 res.innerHTML= `Detectamos uma <strong>${genero} Criança</strong> com <strong>${idade}</strong> anos.`
             }else if(idade < 21){
                 //jovem-adolescente
-                img.src = 'homemadolescente.png'
+                img.src = 'imagens/homemadolescente.png'
                 res.innerHTML= `Detectamos uma <strong>${genero} Adolescente</strong> com <strong>${idade}</strong> anos.`
             }else if(idade < 50){
                 //adulto
-                img.src = 'homemadulto.png'
+                img.src = 'imagens/homemadulto.png'
                 res.innerHTML= `Detectamos uma <strong>${genero} Adulto</strong> com <strong>${idade}</strong> anos.`
             }else {
                //idoso 
-               img.src = 'homemidoso.png'
+               img.src = 'imagens/homemidoso.png'
                res.innerHTML= `Detectamos um <strong>${genero} Idoso</strong> com <strong>${idade}</strong> anos.`
             }
 
@@ -35,19 +35,19 @@ function verificar(){
             genero = 'Mulher'
             if(idade >= 0 && idade < 10){
                 //bebe-criança
-                img.src = 'mulherbebe.png'
+                img.src = 'imagens/mulherbebe.png'
                 res.innerHTML= `Detectamos uma <strong>${genero} Criança</strong> com <strong>${idade}</strong> anos.`
             }else if(idade < 21){
                 //jovem-adolescente
-                img.src = 'mulheradolescente.png'
+                img.src = 'imagens/mulheradolescente.png'
                 res.innerHTML= `Detectamos uma <strong>${genero} adolecesnte</strong> com <strong>${idade}</strong> anos.`
             }else if(idade < 50){
                 //adulto
-                img.src = 'mulheradulta.png'
+                img.src = 'imagens/mulheradulta.png'
                 res.innerHTML= `Detectamos uma <strong>${genero} Adulta</strong> com <strong>${idade}</strong> anos.`
             }else {
                //idoso 
-               img.src = 'mulheridosa.png'
+               img.src = 'imagens/mulheridosa.png'
                res.innerHTML= `Detectamos uma <strong>${genero} Idosa</strong> com <strong>${idade}</strong> anos.`
             }
         
